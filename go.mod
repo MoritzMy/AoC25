3db0964040471f871ecfe0243f212e30b4f97a13
https://gitlab.isoad.isogmbh.de/MMR <moritz.meyer@iso-gruppe.com>
2025-12-01 17:01:01 +0100

    updated day1 solution

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module untitled
+module AoC2025
 
 go 1.25
--- util/FileReader.go
+++ util/FileReader.go
@@ -1,1 +1,29 @@
 package util
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func readFile(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var lines []string
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	return lines
+}
